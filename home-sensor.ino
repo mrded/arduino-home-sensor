f1f637b718a07a8d5a4bee18c7bf4842cda17f2a
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-08-16 00:05:15 +0100

    Show result on a screen.

--- home-sensor.ino
+++ home-sensor.ino
@@ -1,20 +1,14 @@
-// This #include statement was automatically added by the Particle IDE.
-#include "Adafruit_IO_Client.h"
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
-// This #include statement was automatically added by the Particle IDE.
-#include "Adafruit_IO_Arduino.h"
+#include <Adafruit_IO_Client.h>
+#include <Adafruit_IO_Arduino.h>
 
-// This #include statement was automatically added by the Particle IDE.
-#include "DHT.h"
-
-// AdaFruit.io
-#include "Adafruit_IO_Client.h"
-#include "Adafruit_IO_Arduino.h"
+#include <DHT.h>
 
 //
 // Includes
 //
-#include "DHT.h"
 #include "application.h"
 
 //
@@ -56,6 +50,11 @@
 #define PARTICLE_EVENT 0
 #define PARTICLE_EVENT_NAME "dht-logger-log"
 
+// Use I2C with OLED RESET pin on D4
+// Example: https://github.com/adafruit/Adafruit_SSD1306/blob/master/examples/ssd1306_128x32_i2c/ssd1306_128x32_i2c.ino
+#define OLED_RESET D4
+Adafruit_SSD1306 display(OLED_RESET);
+
 //
 // Integration Includes
 //
@@ -92,91 +91,116 @@ char payload[1024];
  * Setup
  */
 void setup() {
-    pinMode(LEDPIN, OUTPUT);
-    digitalWrite(LEDPIN, HIGH);
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+  display.display();
 
-    // configure Particle variables - float isn't accepted, so we have to use string versions
-    Particle.variable("temperature", &temperatureString[0], STRING);
-    Particle.variable("humidity", &humidityString[0], STRING);
-    Particle.variable("heatIndex", &heatIndexString[0], STRING);
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, HIGH);
 
-    Particle.variable("status", &failed, INT);
+  // configure Particle variables - float isn't accepted, so we have to use string versions
+  Particle.variable("temperature", &temperatureString[0], STRING);
+  Particle.variable("humidity", &humidityString[0], STRING);
+  Particle.variable("heatIndex", &heatIndexString[0], STRING);
 
-    // start the DHT sensor
-    dht.begin();
+  Particle.variable("status", &failed, INT);
 
-    aio.begin();
+  // start the DHT sensor
+  dht.begin();
+
+  aio.begin();
 
 #if PARTICLE_EVENT
-    // startup event
-    sprintf(payload,
-            "{\"device\":\"%s\",\"state\":\"starting\"}",
-            DEVICE_NAME);
+  // startup event
+  sprintf(payload,
+      "{\"device\":\"%s\",\"state\":\"starting\"}",
+      DEVICE_NAME);
 
-    Spark.publish(PARTICLE_EVENT_NAME, payload, 60, PRIVATE);
+  Spark.publish(PARTICLE_EVENT_NAME, payload, 60, PRIVATE);
 #endif
 
-    // run the first measurement
-    loop();
+  // run the first measurement
+  loop();
 }
 
 /**
  * Event loop
  */
 void loop() {
-    int now = Time.now();
+  int now = Time.now();
 
-    // only run every SEND_INTERVAL seconds
-    if (now - lastUpdate < SEND_INTERVAL) {
-        return;
-    }
+  // only run every SEND_INTERVAL seconds
+  if (now - lastUpdate < SEND_INTERVAL) {
+    return;
+  }
 
-    // turn on LED when updating
-    digitalWrite(LEDPIN, HIGH);
+  // turn on LED when updating
+  digitalWrite(LEDPIN, HIGH);
 
-    lastUpdate = now;
+  lastUpdate = now;
 
+  failed = 0;
+
+  // read humidity and temperature values
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature(USE_FARENHEIT);
+
+  if (temperature == NAN
+      || humidity == NAN
+      || temperature > MAX_TEMPERATURE
+      || temperature < MIN_TEMPERATURE
+      || humidity > MAX_HUMIDITY
+      || humidity < MIN_HUMIDITY) {
+    // if any sensor failed, bail on updates
+    failed = 1;
+  } else {
     failed = 0;
 
-    // read humidity and temperature values
-    humidity = dht.readHumidity();
-    temperature = dht.readTemperature(USE_FARENHEIT);
+    // calculate the heat index
+    heatIndex = dht.computeHeatIndex(temperature, humidity, USE_FARENHEIT);
 
-    if (temperature == NAN
-        || humidity == NAN
-        || temperature > MAX_TEMPERATURE
-        || temperature < MIN_TEMPERATURE
-        || humidity > MAX_HUMIDITY
-        || humidity < MIN_HUMIDITY) {
-        // if any sensor failed, bail on updates
-        failed = 1;
-    } else {
-        failed = 0;
+    // convert floats to strings for Particle variables
+    sprintf(temperatureString, "%.2f", temperature);
+    sprintf(humidityString, "%.2f", humidity);
+    sprintf(heatIndexString, "%.2f", heatIndex);
 
-        // calculate the heat index
-        heatIndex = dht.computeHeatIndex(temperature, humidity, USE_FARENHEIT);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
 
-        // convert floats to strings for Particle variables
-        sprintf(temperatureString, "%.2f", temperature);
-        sprintf(humidityString, "%.2f", humidity);
-        sprintf(heatIndexString, "%.2f", heatIndex);
+    display.print("temperature: ");
+    display.println(temperature);
 
-        aioFeedTemperature.send(temperature);
-        aioFeedHumidity.send(humidity);
-        aioFeedHeatIndex.send(heatIndex);
+    display.print("humidity: ");
+    display.println(humidity);
 
-        sprintf(payload,
-            "{\"device\":\"%s\",\"temperature\":%.2f,\"humidity\":%.2f,\"heatIndex\":%.2f}",
-            DEVICE_NAME,
-            temperature,
-            humidity,
-            heatIndex);
+    display.print("heatIndex: ");
+    display.println(heatIndex);
+
+    display.display();
+
+    aioFeedTemperature.send(temperature);
+    aioFeedHumidity.send(humidity);
+    aioFeedHeatIndex.send(heatIndex);
+
+    sprintf(payload,
+        "{\"device\":\"%s\",\"temperature\":%.2f,\"humidity\":%.2f,\"heatIndex\":%.2f}",
+        DEVICE_NAME,
+        temperature,
+        humidity,
+        heatIndex);
 
 #if PARTICLE_EVENT
-        Spark.publish(PARTICLE_EVENT_NAME, payload, 60, PRIVATE);
+    Spark.publish(PARTICLE_EVENT_NAME, payload, 60, PRIVATE);
 #endif
-    }
+  }
 
-    // done updating
-    digitalWrite(LEDPIN, LOW);
+  // done updating
+  digitalWrite(LEDPIN, LOW);
 }
