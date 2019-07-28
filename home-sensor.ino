ade7544ef6cc9e4bbd671fe02ba73a92c9b282e8
Dmitry Demenchuk <dmitry@demenchuk.me>
2019-07-28 23:23:15 +0100

    Cleanup, clean screen on reload, calibrate co2 sensor on setup.

--- home-sensor.ino
+++ home-sensor.ino
@@ -11,28 +11,13 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 
 DHT dht(DHTPIN, DHTTYPE);
 
-int uptime;
+int uptime = 0;
 
 void setup() {
   lcd.init();
   lcd.backlight();
 
-  lcd.setCursor(0,0);
-  lcd.print("Temperature:");
-
-  lcd.setCursor(3,1);
-  lcd.print("Humidity:");
-
-  lcd.setCursor(8,2);
-  lcd.print("CO2:");
-
-  lcd.setCursor(5,3);
-  lcd.print("Uptime:");
-
-  dht.begin();
   co2Sensor.calibrate();
-
-  uptime = 0;
 }
 
 void loop() {
@@ -40,17 +25,19 @@ void loop() {
   String temperature = String(dht.readTemperature());
   String humidity = String(dht.readHumidity());
 
-  lcd.setCursor(13,0);
-  lcd.print(temperature + " C");
+  lcd.clear();
 
-  lcd.setCursor(13,1);
-  lcd.print(humidity + " %");
+  lcd.setCursor(0,0);
+  lcd.print("Temperature: " + temperature + " C");
 
-  lcd.setCursor(13,2);
-  lcd.print(co2 + " ppm");
+  lcd.setCursor(3,1);
+  lcd.print("Humidity: " + humidity + " %");
 
-  lcd.setCursor(13,3);
-  lcd.print(String(uptime++) + " min");
+  lcd.setCursor(8,2);
+  lcd.print("CO2: " + co2 + " ppm");
+
+  lcd.setCursor(5,3);
+  lcd.print("Uptime: " + String(uptime++) + " min");
   
   delay(60000);
 }
